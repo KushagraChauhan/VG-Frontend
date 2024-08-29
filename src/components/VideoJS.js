41da0b6ca4c61ee12a057a64df8945bae7b94def
Kushagra Chauhan <kushagra_sc@yahoo.in>
2024-08-29 16:50:41 +0530

    new: add timestamp marker in videos of Sections

--- src/components/VideoJS.js
+++ src/components/VideoJS.js
@@ -1,53 +1,59 @@
 import React, { useEffect, useRef } from 'react';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
+import 'videojs-markers/dist/videojs.markers.css';  // Add marker CSS
+import 'videojs-markers';  // Import the markers plugin
 
 export const VideoJS = (props) => {
   const videoRef = useRef(null);
   const playerRef = useRef(null);
-  const { options, onReady } = props;
+  const { options, timestamps, onReady } = props;
 
   useEffect(() => {
-    // Make sure Video.js player is only initialized once
     if (!playerRef.current) {
-      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
       const videoElement = document.createElement('video-js');
       videoElement.classList.add('vjs-big-play-centered');
       videoRef.current.appendChild(videoElement);
 
       const player = (playerRef.current = videojs(videoElement, options, () => {
-        // videojs.log('player is ready');
         onReady && onReady(player);
 
-        // Load the saved time from localStorage
         const videoKey = `video-${options.sources[0].src}`;
         const savedTime = localStorage.getItem(videoKey);
         if (savedTime) {
           player.currentTime(parseFloat(savedTime));
-          // videojs.log(`Loaded saved time: ${savedTime}`);
         }
 
-        // Save the current time to localStorage on timeupdate
         player.on('timeupdate', () => {
           const currentTime = player.currentTime();
           localStorage.setItem(videoKey, currentTime);
-          // videojs.log(`Saved time: ${currentTime}`);
         });
 
-        //Disable right-click context menu
         videoElement.addEventListener('contextmenu', (e) => {
           e.preventDefault();
         });
+
+        if (timestamps && timestamps.length > 0) {
+          player.markers({
+            markerStyle: {
+              width: '10px',
+              backgroundColor: '#FFA500',
+            },
+            markers: timestamps.map(({ time, label }) => ({
+              time,
+              text: label,
+              class: 'marker-class',
+            })),
+          });
+        }
       }));
     } else {
-      // Update the existing player
       const player = playerRef.current;
       player.autoplay(options.autoplay);
       player.src(options.sources);
     }
-  }, [options, onReady]);
+  }, [options, onReady, timestamps]);
 
-  // Dispose the Video.js player when the functional component unmounts
   useEffect(() => {
     const player = playerRef.current;
 
