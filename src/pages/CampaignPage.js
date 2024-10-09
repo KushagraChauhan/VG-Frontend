55719cde027eb6817a5fb2e95acfe20986c3feaa
Kushagra Chauhan <kushagra_sc@yahoo.in>
2024-10-09 12:12:48 +0530

    new: add static courseRecommendation Component

--- src/pages/CampaignPage.js
+++ src/pages/CampaignPage.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import LPHeader from "../components/LPHeader";
 import Campaigns from "../components/Campaigns";
 import Footer from "../components/Footer";
+import CourseRecommendation from "../components/CourseRecommendation";
 
 const useAuth = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -18,12 +19,20 @@ const useAuth = () => {
     return isAuthenticated;
   };
 
+  const course = {
+    title: "Breaking myths: Status of women in Vedas and Manusmriti",
+    description: "Let’s return to our roots, to a culture where Devi is worshipped and women are highly regarded. ",
+    image_url: "https://vibegurukul.s3.ap-south-1.amazonaws.com/preview-image-04.webp",
+    learnings: "Know the value of women & feminine. Expose people talking ill about Bhartiya Sanskriti. Be a true feminist and stand for Dharma."
+  };
+
 const CampaignPage = () => {
     const isAuthenticated = useAuth();
     return(  
     <div>
         {isAuthenticated ? <Header /> : <LPHeader />}
         <Campaigns />
+        <CourseRecommendation course={course}/>
         {/* <Footer /> */}
     </div>
     );
